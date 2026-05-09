8d76f17c0231986018629d9baf0bbfda005f9b34
ncdjknkedah <ncdjkn.kdh@gmail.com>
2026-05-09 21:49:45 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'nhsi-cache-v2';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'nhsi_app_icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
